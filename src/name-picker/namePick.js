c74a28f1d1632cf82efa2b4ed4df75060d5fc316
fermich <fermich11@hotmail.com>
2021-09-30 18:16:34 -0500

    Re-branding page

--- src/name-picker/namePick.js
+++ src/name-picker/namePick.js
@@ -3,58 +3,56 @@ let first = '';
 let second = '';
 let third = '';
 
-$('#pick').click(function() {
-    // Get the input value
-  var url = document.getElementById('url');
-  if(url.value.includes('http')) {
-    // Seperate the url and push them into the array
-    var nameArray = url.value.split(',');
-    
-    // Get a random name, the winner
-    let winners = ['', '', ''];
-    if (first != '') {
-      winners[2] = first;
-      const newArray = nameArray.filter(name => name != first);
-      nameArray = newArray;
-    } else if(second != '') {
-      winners[1] = second;
-      const newArray = nameArray.filter(name => name != second);
-      nameArray = newArray;
-    } else if(third != '') {
-      winners[0] = third;
-      const newArray = nameArray.filter(name => name != third);
-      nameArray = newArray;
-    }
-
-    for (let count = 0; count <= 2 ; count++ ) {
-      if(winners[count] == '') {
-        const winnerName = nameArray[Math.floor(Math.random()*nameArray.length)];
-        winners[count] = winnerName;
-        const newArray = nameArray.filter(name => name != winnerName);
-        nameArray = newArray;
-      }
-    }
-
-    winners = winners.reverse();
-    
-    winner = `<span>🎉 The winners are 🎉</span><br><h1>🥇 ${first}</h1><br><h2>🥈 ${second}<h2><br><h3>🥉 ${third}<h3>`;
-    
-    // Display winner
-    $('#world').addClass('open');
-    $('#winner').addClass('open');
-    $('#close').addClass('open');
-    $('#winner').html(winner);
-  } else {
-    url.style.color = 'red';
-    const errorMessage = document.createElement('span');
-    errorMessage.style.color = 'red';
-    errorMessage.style.margin = '5px';
-    errorMessage.style.paddingLeft = '1vh';
-    errorMessage.textContent = 'Invalid Url';
-    document.querySelector('#inputArea').appendChild(errorMessage);
-  }
-  
+$('#pick').click(() => {
+  const spinner = document.querySelector('.spinner-area');
+  spinner.style.display = 'block';
+  setTimeout(() => {
+    spinner.style.display = 'none';
+    setWinners();
+  }, 3000);
 });
+  
+function setWinners() {
+  // Get the input value
+  var url = document.getElementById('url');
+  // Seperate the url and push them into the array
+  var nameArray = url.value.split('\n');
+  
+  // Get a random name, the winner
+  let winners = ['', '', ''];
+  if (first != '') {
+    winners[2] = first;
+    const newArray = nameArray.filter(name => name != first);
+    nameArray = newArray;
+  } else if(second != '') {
+    winners[1] = second;
+    const newArray = nameArray.filter(name => name != second);
+    nameArray = newArray;
+  } else if(third != '') {
+    winners[0] = third;
+    const newArray = nameArray.filter(name => name != third);
+    nameArray = newArray;
+  }
+
+  for (let count = 0; count <= 2 ; count++ ) {
+    if(winners[count] == '') {
+      const winnerName = nameArray[Math.floor(Math.random()*nameArray.length)];
+      winners[count] = winnerName;
+      const newArray = nameArray.filter(name => name != winnerName);
+      nameArray = newArray;
+    }
+  }
+
+  winners = winners.reverse();
+  
+  winner = `<span>🎉 The winners are 🎉</span><br><h1>🥇 ${winners[0]}</h1><br><h2>🥈 ${winners[1]}<h2><br><h3>🥉 ${winners[2]}<h3>`;
+  
+  // Display winner
+  $('#world').addClass('open');
+  $('#winner').addClass('open');
+  $('#close').addClass('open');
+  $('#winner').html(winner);
+};
   
 $('#close').click(function() {
   $('#world').removeClass('open');
